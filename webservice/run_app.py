fd276d5c19ce6acdf23e301251457d0bbf143edb
Giovanni Pizzi <giovanni.pizzi@epfl.ch>
2018-07-19 15:09:27 +0200

    Allowing to import python code from 'compute' submodule

--- webservice/run_app.py
+++ webservice/run_app.py
@@ -285,53 +285,12 @@ def send_fonts(path):
     """
     return flask.send_from_directory(os.path.join(static_folder, 'fonts'), path)
 
+try:
+    from compute import blueprint
+    app.register_blueprint(blueprint)
+except ImportError:
+    print("WARNING: could not import the 'compute' module with custom functions")
 
-@app.route('/process_data/', methods=['GET', 'POST'])
-def process_data():
-    """
-    Process data (uploaded from POST request)
-    """
-    if flask.request.method == 'POST':
-        try:
-            data_for_template = process_data_core(
-                module_version="",
-                call_source="process_data",
-                logger=logger,
-                flask_request=flask.request)
-            config_dict = get_config()
-            return flask.render_template(
-                get_visualizer_template(flask.request), config=config_dict['config'], **data_for_template)
-        except FlaskRedirectException as e:
-            flask.flash(str(e))
-            return flask.redirect(flask.url_for('input_data'))
-        except Exception:
-            flask.flash("Unable to process the data, sorry...")
-            return flask.redirect(flask.url_for('input_data'))
-
-    else:  # GET Request
-        return flask.redirect(flask.url_for('input_data'))
-
-
-@app.route('/process_example_data/', methods=['GET', 'POST'])
-def process_example_data():
-    """
-    Process an example data file (example name from POST request)
-    """
-    if flask.request.method == 'POST':
-        try:
-            data_for_template = process_data_core(
-                module_version="",
-                call_source="process_example_data",
-                logger=logger,
-                flask_request=flask.request)
-            return flask.render_template(
-                get_visualizer_template(flask.request), config=config, **data_for_template)
-        except FlaskRedirectException as e:
-            flask.flash(str(e))
-            return flask.redirect(flask.url_for('input_data'))
-
-    else:  # GET Request
-        return flask.redirect(flask.url_for('input_data'))
 
 
 if __name__ == "__main__":
